d8acb3e4ead99734bcebb3f62dafa5415373260a
Konstantin Zaporozhtsev <51118814+octopusden@users.noreply.github.com>
2023-09-28 16:58:13 +0300

    Update CallGitHubReleaseTask.kt

--- teamcity/scripts/CallGitHubReleaseTask.kt
+++ teamcity/scripts/CallGitHubReleaseTask.kt
@@ -5,24 +5,31 @@ import khttp.post
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
 
-val respCreate = post("https://api.github.com/repos/octopusden/${args[0]}/dispatches", mapOf("Authorization" to "Bearer ${args[1]}"), emptyMap(), """{
-        "event_type": "${args[5]}",
+val octopusModule = args[0]
+val githubToken = args[1]
+val currentCommit = args[2]
+val versionToRelease = args[3]
+val timeoutMinutes = args[4]
+val eventType = args[5]
+
+val respCreate = post("https://api.github.com/repos/octopusden/$octopusModule/dispatches", mapOf("Authorization" to "Bearer $githubToken"), emptyMap(), """{
+        "event_type": "$eventType",
         "client_payload": {
-        "commit": "${args[2]}",
-        "project_version": "${args[3]}"
+        "commit": "$currentCommit",
+        "project_version": "$versionToRelease"
     }""")
 if (respCreate.statusCode / 100 != 2) {
     throw Exception(respCreate.text)
 }
 println("Waiting for release to complete...")
 var attempt = 1
-var limit = Integer.valueOf(args[4])
+var limit = Integer.valueOf(timeoutMinutes)
 do {
-	   println("Attempt: " + attempt)
-     if (attempt > limit) {
-         throw TimeoutException("Number of attempts exceeded($attempt)")
-     }
-     TimeUnit.MINUTES.sleep(1L)
-     val respCheck = get(url = "https://api.github.com/repos/octopusden/${args[0]}/releases/tags/v${args[3]}")
-	   attempt++
+    println("Attempt: " + attempt)
+    if (attempt > limit) {
+        throw TimeoutException("Number of attempts exceeded($attempt)")
+    }
+    TimeUnit.MINUTES.sleep(1L)
+    val respCheck = get(url = "https://api.github.com/repos/octopusden/$octopusModule/releases/tags/v$versionToRelease")
+    attempt++
 } while (respCheck.statusCode / 100 != 2)
