8afcd664d21ef1c636529eeb655fd6857915f8ba
Konstantin Zaporozhtsev <51118814+octopusden@users.noreply.github.com>
2023-09-28 16:34:12 +0300

    Update HelloWorld.main.kts

--- teamcity/scripts/HelloWorld.main.kts
+++ teamcity/scripts/HelloWorld.main.kts
@@ -1,1 +1,11 @@
+import org.apache.maven.artifact.versioning.ComparableVersion
+
 println("Hello World!")
+
+val version1 = ComparableVersion("1.2.3")
+val version2 = ComparableVersion("1.3.4")
+
+println(version1)
+println(version2)
+
+println(version1.compareTo(version2))
